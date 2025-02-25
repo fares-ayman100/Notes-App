f883d1c948b9903e31194f728b0d83f6eecd9f5b
Fares Ayman <fareshe73@gmail.com>
2025-02-26 00:13:24 +0200

    add a extract notes list view

--- lib/Widget/notes_view_body.dart
+++ lib/Widget/notes_view_body.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:notes_app/Widget/custom_appbar.dart';
 import 'package:notes_app/Widget/custom_notes_item.dart';
+import 'package:notes_app/Widget/notes_list_view.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
@@ -10,7 +11,13 @@ class NotesViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 32, horizontal: 16),
-      child: Column(children: [CustomAppBar(), NoteItem()]),
+      child: Column(
+        children: [
+          SizedBox(height: 25),
+          CustomAppBar(),
+          Expanded(child: NotesListView()),
+        ],
+      ),
     );
   }
 }
