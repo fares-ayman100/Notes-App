45643cff99be9487de56e75384617a0b8fd44f81
Fares Ayman <fareshe73@gmail.com>
2025-03-04 19:55:25 +0200

    fetch all notes method in cubit

--- lib/Views/notes_view.dart
+++ lib/Views/notes_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/Cubit/notes/notes_cubit.dart';
 import 'package:notes_app/Widget/add_note_buttom_sheet.dart';
 import 'package:notes_app/Widget/notes_view_body.dart';
 import 'package:notes_app/constant.dart';
@@ -8,27 +10,31 @@ class NotesView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: NotesViewBody(),
-      floatingActionButton: SizedBox(
-        height: 65,
-        width: 65,
-        child: FloatingActionButton(
-          shape: CircleBorder(),
-          backgroundColor: primaryColor,
-          onPressed: () {
-            showModalBottomSheet(
-              isScrollControlled: true,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(16),
-              ),
-              context: context,
-              builder: (context) {
-                return AddNoteButtomSheet();
-              },
-            );
-          },
-          child: Icon(Icons.add, color: Colors.black),
+    return BlocProvider(
+      create: (context) => NotesCubit(),
+      child: Scaffold(
+        body: NotesViewBody(),
+        resizeToAvoidBottomInset: false,
+        floatingActionButton: SizedBox(
+          height: 65,
+          width: 65,
+          child: FloatingActionButton(
+            shape: CircleBorder(),
+            backgroundColor: primaryColor,
+            onPressed: () {
+              showModalBottomSheet(
+                isScrollControlled: true,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                context: context,
+                builder: (context) {
+                  return AddNoteButtomSheet();
+                },
+              );
+            },
+            child: Icon(Icons.add, color: Colors.black),
+          ),
         ),
       ),
     );
