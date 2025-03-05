aeaa1b9c66b1e66f28dc07a27518ea301b4e36fb
Fares Ayman <fareshe73@gmail.com>
2025-03-05 17:43:00 +0200

    colors list view done

--- lib/Widget/colors-list-view.dart
+++ lib/Widget/colors-list-view.dart
@@ -1,30 +1,63 @@
 import 'package:flutter/material.dart';
 
 class ColorItem extends StatelessWidget {
-  const ColorItem({super.key});
+  const ColorItem({super.key, required this.isActive, required this.color});
+  final Color color;
 
   @override
+  final bool isActive;
+
   Widget build(BuildContext context) {
-    return CircleAvatar(backgroundColor: Colors.red, radius: 38);
+    return isActive
+        ? CircleAvatar(
+          radius: 40,
+          backgroundColor: Colors.white,
+          child: CircleAvatar(backgroundColor: color, radius: 35),
+        )
+        : CircleAvatar(backgroundColor: color, radius: 40);
   }
 }
 
-class CustomListView extends StatelessWidget {
+class CustomListView extends StatefulWidget {
   const CustomListView({super.key});
 
   @override
+  State<CustomListView> createState() => _CustomListViewState();
+}
+
+class _CustomListViewState extends State<CustomListView> {
+  List<Color> colors = [
+    Color(0xffFAD4D8),
+    Color(0xffB4CEB3),
+    Color(0xffE2A3C7),
+    Color(0xffA8DCD9),
+    Color(0xff9BB291),
+    Color(0xff235789),
+    Color(0xffE4E6C3),
+  ];
+  int currentSelect = 0;
+  @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 38 * 2,
+      height: 40 * 2,
       child: ListView.separated(
         scrollDirection: Axis.horizontal,
         itemBuilder: (context, index) {
-          return ColorItem();
+          return GestureDetector(
+            onTap: () {
+              currentSelect = index;
+              setState(() {});
+            },
+            child: ColorItem(
+              isActive: currentSelect == index,
+              color: colors[index],
+            ),
+          );
         },
         separatorBuilder: (context, index) {
           return SizedBox(width: 10);
         },
-        itemCount: 10,
+        itemCount: colors.length,
       ),
     );
   }
